583846f92d632065b7a374666a6f62fcf01dc305
arsgomes-dev <arsgomes-dev@gmail.com>
2024-05-16 21:47:30 -0300

    update

--- Meu primeiro projeto java/src/MinhaClasse.java
+++ Meu primeiro projeto java/src/MinhaClasse.java
@@ -1,5 +1,12 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        String primeiroNome = "Ricardo";
+        String segundoNome = "Gomes";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
