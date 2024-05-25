4cb6485ae55bf6abc0473ef5d0295d6f1c4e0508
arsgomes-dev <arsgomes-dev@gmail.com>
2024-05-24 22:31:30 -0300

    BankAccount

--- Bank-Account/src/TerminalAccount.java
+++ Bank-Account/src/TerminalAccount.java
@@ -1,3 +1,10 @@
-public class TerminalAccount {
+import java.util.Locale;
+import java.util.Scanner;
 
+public class TerminalAccount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        scanner.close();
+    }
 }
