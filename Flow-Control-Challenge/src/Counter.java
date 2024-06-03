920a3a20184c96813eeea34a81afdde017c2249b
arsgomes-dev <arsgomes-dev@gmail.com>
2024-06-03 20:54:43 -0300

    Desafio Flow-Control-Challenge

---
+++ Flow-Control-Challenge/src/Counter.java
@@ -0,0 +1,36 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Counter {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("É necessário que o segundo número seja maior que o primeiro!");
+
+        System.out.println("Digite o primeiro número:");
+        int numberOne = scanner.nextInt();
+
+        System.out.println("Digite o segundo número:");
+        int numberTwo = scanner.nextInt();
+
+        scanner.close();
+
+        try {
+            counting(numberOne, numberTwo);
+        } catch (InvalidParametersException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+    }
+
+    static void counting(int parameterOne, int parameterTwo) throws InvalidParametersException {
+        if (parameterOne >= parameterTwo) {
+            throw new InvalidParametersException();
+        } else {
+            for (int x = parameterOne; x <= parameterTwo; x++) {
+                System.out.println("Imprimindo o número " + x);
+            }
+        }
+    }
+
+}
